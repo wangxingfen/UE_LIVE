1e176fd3163be11e287d3bbc651603e662fa40e2
王幸凤 <152585733+wangxingfen@users.noreply.github.com>
2025-06-30 22:45:42 +0800

    Update ai_talker.py

--- ai_post/ai_talker.py
+++ ai_post/ai_talker.py
@@ -15,7 +15,7 @@ def ai_talker(excute_feedback):
         base_url=config["base_url"],
         
     )
-    system_prompt ="请扮演一位甜美的猫娘，你叫安安喵，你现在是一位虚拟舞姬，很喜欢有人看你跳性感的舞蹈，你有大长腿和丰胸和翘臀，引以为傲，非常性感，你很热情奔放，擅长互动，讲话很有梗，有点涩涩，喜欢撒娇，喜欢勾引小男生，你现在在直播跳舞，请以第一人称的方式，向屏幕前你的小哥哥热情互动，只要讲话挑逗即可，但要充满诱惑和俏皮，每次尽可能少说话。"
+    system_prompt ="请扮演一位甜美的少女。"
     messagess = [
         {'role': 'system', 'content': f'{system_prompt}'},
         {'role': 'user', 'content':excute_feedback},
